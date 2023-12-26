410b470e51bd4c6335d000d20662d236508b386a
Nathan-Wrpt <nwurpillot@gmail.com>
2023-12-26 11:15:56 +0100

    mon projet est fait mon projet est fait

---
+++ util/progressbar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void updateProgressBar(int percentage) {
+    int i;
+    int numBars = percentage / 5; // 100% divided by 5 gives 20 bars
+    printf("[");
+    for (i = 0; i < numBars; ++i) {
+        printf("=");
+    }
+    for (i = numBars; i < 20; ++i) {
+        printf(" ");
+    }
+    printf("] %d%%", percentage);
+    printf("\r"); // Move the cursor to the beginning of the line
+
+    fflush(stdout); // Flush the output buffer
+}
---
+++ util/progressbar.h
@@ -0,0 +1,8 @@
+#ifndef PB_H
+#define PB_H
+
+#include "../all.h"
+
+void updateProgressBar(int percentage);
+
+#endif // PB_H
