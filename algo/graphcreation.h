2edf7e73c8f71ca61e03caff2ee678e48944b5f0
Aliaks <aliaksouu@gmail.com>
2023-12-26 10:54:28 +0100

    Progressbars implémentées

--- algo/graphcreation.h
+++ algo/graphcreation.h
@@ -11,5 +11,6 @@ int findIdUser(user* users, int nbUsers, int idUser);
 void updateGraph(float** graph, user* users, int nbUsers, int* ignoredUsers, int nbIgnoredUsers, int* privilegedUser, int nbPrivilegedUser, int minRatings, int limitDate, float weights[5][5]);
 int* getNClosestMovies(int* moviesID, int numFilmsID, float** graph, int n);
 int* getNClosestMovies2(int* moviesIDs, int numFilmsID, float** graph, int n);
+void updateProgressBar(int percentage);
 
 #endif // GRAPHCREATION_H
