b8449c10c1c5a75f0ccdc2e5d8fc82b5475d1a18
wurpillot <nathan.wurpillot@etu.unistra.fr>
2024-01-04 16:14:58 +0100

    Amélioration du README

--- util/maxadvices.h
+++ util/maxadvices.h
@@ -9,4 +9,10 @@ int countLines(const char* filename);
 //Returns the max numbers of ratings a user has given
 int getMaxAdvices(char* folderpath);
 
+//Returns the maximum number of ratings a user has given
+int maxratings(user* users, int numUsers);
+
+//Returns the average number of ratings the users gave
+float averageRatings(user* users, int numUsers);
+
 #endif // MAXA_H
